fbc1c3f3815ebc4a59ab69aea9a455f223224a11
LeesAwesomePC <LeesAwesomePC@LeeMeally.station>
2020-10-29 15:34:09 +0000

    Fully completed

---
+++ Labs/src/week4/lab3/Clock.java
@@ -0,0 +1,34 @@
+package week4.lab3;
+
+import java.util.Calendar;
+
+public class Clock {
+
+	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance();
+		Time t = new Time (cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE));
+	
+		System.out.println(t);
+		
+		long startTime = System.currentTimeMillis();
+		long currentTime = startTime;
+		int oldMin = cal.get(Calendar.MINUTE);
+		int newMin = oldMin;
+		while(newMin == oldMin) {
+			while((currentTime - startTime)<1000) //wait 1 second before calling tick method
+		{
+			currentTime = System.currentTimeMillis();
+			
+		}
+			t.tick();
+			System.out.println(t.toString());
+			newMin = t.getMinute();
+			startTime = System.currentTimeMillis();
+			currentTime = startTime;
+			
+			
+		}
+		
+	}
+
+}
