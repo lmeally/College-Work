3c9763b6c5e72f3d3c6cdba96ab638f877f83931
LeesAwesomePC <LeesAwesomePC@LeeMeally>
2021-02-10 15:58:41 +0000

    Lab 12 & 13

---
+++ Labs/src/week15/lab13_/ColourSelect.java
@@ -0,0 +1,17 @@
+package week15.lab13_;
+
+import javax.swing.JFrame;
+
+public class ColourSelect {
+
+	public static void main(String[] args) {
+		
+		ColourSelectFrame colourSelectFrame = new ColourSelectFrame();
+		colourSelectFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		colourSelectFrame.setSize(300, 125);
+		colourSelectFrame.setLocation(500,400);
+		colourSelectFrame.setVisible(true);
+
+	}
+
+}
